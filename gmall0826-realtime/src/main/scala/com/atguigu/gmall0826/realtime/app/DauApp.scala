1dd609770534b2d25ee89cf47ec6c26ba7ad92ed
zhangchen <windyzj@163.com>
2020-02-08 11:15:30 +0800

    实时处理去重完成

--- gmall0826-realtime/src/main/scala/com/atguigu/gmall0826/realtime/app/DauApp.scala
+++ gmall0826-realtime/src/main/scala/com/atguigu/gmall0826/realtime/app/DauApp.scala
@@ -1,14 +1,17 @@
 package com.atguigu.gmall0826.realtime.app
 
 import java.text.SimpleDateFormat
+import java.util
 import java.util.Date
 
 import com.alibaba.fastjson.JSON
 import com.atguigu.gmall0826.common.constant.GmallConstant
 import com.atguigu.gmall0826.realtime.bean.StartupLog
-import com.atguigu.gmall0826.realtime.util.MyKafkaUtil
+import com.atguigu.gmall0826.realtime.util.{MyKafkaUtil, RedisUtil}
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.spark.SparkConf
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import redis.clients.jedis.Jedis
@@ -48,15 +51,77 @@ object DauApp {
     //2  去重   保留每个mid当日的第一条   其他的启动日志过滤掉
 
     //  然后再利用清单进行过滤筛选 把清单中已有的用户的新日志过滤掉
+/* 方案A     连接redis次数太多  可以进一步优化
+ val filteredDstream: DStream[StartupLog] = startUpLogDstream.filter { startuplog =>
+      val jedis: Jedis = RedisUtil.getJedisClient
+      val dauKey = "dau:" + startuplog.logDate
+      val flag = !jedis.sismember(dauKey, startuplog.mid)
+      jedis.close()
+      flag
+    }*/
+    // 优化：  利用driver查询出完整清单，然后利用广播变量发送给各个executor
+    // 各个ex  利用广播变量中的清单  检查自己的数据是否需要过滤 在清单中的一律清洗掉
+    //1  查 driver
+  /*  方案B 错误代码  会造成driver只执行了一次  不会周期性执行
+    val jedis: Jedis = RedisUtil.getJedisClient
+    val today: String = new SimpleDateFormat("yyyy-MM-dd").format(new Date())
+    val dauKey="dau:"+today
+    val midSet: util.Set[String] = jedis.smembers(dauKey)
+    jedis.close()
+  // 2  发
+    val midBC: Broadcast[util.Set[String]] = ssc.sparkContext.broadcast(midSet)
+
+    startUpLogDstream.filter{startuplog=>  //3 ex 收   筛查
+      val midSet: util.Set[String] = midBC.value
+      !midSet.contains(startuplog.mid)
+    }*/
+
+    //方案C
+    //让driver 每5秒执行一次
+    val filteredDstream: DStream[StartupLog] = startUpLogDstream.transform { rdd =>
+      //driver 每5秒执行一次
+      //1  查 driver
+      println("过滤前：" + rdd.count())
+      val jedis: Jedis = RedisUtil.getJedisClient
+      val today: String = new SimpleDateFormat("yyyy-MM-dd").format(new Date())
+      val dauKey = "dau:" + today
+      val midSet: util.Set[String] = jedis.smembers(dauKey)
+      jedis.close()
+      // 2  发
+      val midBC: Broadcast[util.Set[String]] = ssc.sparkContext.broadcast(midSet)
+
+      val filteredRDD: RDD[StartupLog] = rdd.filter { startuplog =>
+        //3 ex 收   筛查
+        val midSet: util.Set[String] = midBC.value
+        !midSet.contains(startuplog.mid)
+      }
+      println("过滤后：" + filteredRDD.count())
+      filteredRDD
+      // driver
+    }
+
+    //  自检内部  分组去重 ： 0 转换成kv  1   先按mid进行分组   2 组内排序 按时间排序  3  取 top1
+    val groupbyMidDstream: DStream[(String, Iterable[StartupLog])] = filteredDstream.map(startuplog=>(startuplog.mid,startuplog)).groupByKey()
+    val realFilteredDstream: DStream[StartupLog] = groupbyMidDstream.map { case (mid, startuplogItr) =>
+      val startuplogList: List[StartupLog] = startuplogItr.toList.sortWith { (startuplog1, startuplog2) =>
+        startuplog1.ts < startuplog2.ts // 正序排序
+      }
+      val top1startupLogList: List[StartupLog] = startuplogList.take(1)
+      top1startupLogList(0)
+    }
+
+
 
 
     //  利用redis保存当日访问过的用户清单
-    startUpLogDstream.foreachRDD{rdd=>
-
+    realFilteredDstream.foreachRDD{rdd=>
       rdd.foreachPartition{ startupLogItr=>
-        val jedis = new Jedis("hadoop1",6379)   // ex
+        val jedis: Jedis = RedisUtil.getJedisClient
+       // val jedis = new Jedis("hadoop1",6379)   // ex
         for ( startupLog <- startupLogItr ) {
           val dauKey="dau:"+startupLog.logDate
+          // type  set     // key    dau:2020-02-08     //value  mid
+          println(startupLog)
           jedis.sadd(dauKey,startupLog.mid)
           jedis.expire(dauKey,60*60*24);
         }
