44aab3bd4cea647244b66914c3e5dcb7a27ab0c2
zhangchen <windyzj@163.com>
2020-02-15 16:55:33 +0800

    预警完成

--- gmall0826-canal/src/main/java/com/atguigu/gmall0826/canal/CanalHandler.java
+++ gmall0826-canal/src/main/java/com/atguigu/gmall0826/canal/CanalHandler.java
@@ -23,12 +23,15 @@ public class CanalHandler {
 
     public void   handle(){
          if(this.rowDataList!=null &&this.rowDataList.size()>0){
-             //下单业务
+             //下单业务主表
              if(tableName.equals("order_info")&&eventType== CanalEntry.EventType.INSERT) {    // 下订单
                  sendKafka(rowDataList, GmallConstant.KAFKA_TOPIC_ORDER);
+             }else if(tableName.equals("order_detail")&&eventType== CanalEntry.EventType.INSERT){
+                 sendKafka(rowDataList, GmallConstant.KAFKA_TOPIC_ORDER_DETAIL); //下单业务子表
+             }else if(tableName.equals("user_info")&&(eventType== CanalEntry.EventType.INSERT||eventType== CanalEntry.EventType.UPDATE ) ){
+                 sendKafka(rowDataList, GmallConstant.KAFKA_TOPIC_USER); //用户
              }
-             //单据变更
-             //用户新增。。
+
          }
     }
 
@@ -40,8 +43,13 @@ public class CanalHandler {
                 System.out.println(column.getName()+"::"+column.getValue());
                 jsonObject.put(column.getName(),column.getValue());
             }
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             String jsonString = jsonObject.toJSONString();
-            MyKafkaSender.send(GmallConstant.KAFKA_TOPIC_ORDER,jsonString);
+            MyKafkaSender.send(topic,jsonString);
         }
     }
 }
